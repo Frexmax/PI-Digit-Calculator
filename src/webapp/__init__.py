b524eae4a74f34560876af8763660261e363776f
Bartek Nowak <frexmaxxy@gmail.com>
2025-10-26 16:43:38 +0100

    Fix ruff

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -1,6 +1,8 @@
 """File implementing flask web app configuration."""
 from flask import Flask
 
+from .calculate_pi_page import calculate_pi_page
+
 
 def create_app() -> Flask:
     """
@@ -8,8 +10,6 @@ def create_app() -> Flask:
 
     :return: flask web app
     """
-    from .calculate_pi_page import calculate_pi_page
-
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "SECRET_KEY"  # noqa: S105
 
